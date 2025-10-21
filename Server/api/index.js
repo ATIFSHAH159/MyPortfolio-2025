08781e4dadbb2bbb6238ed3d84c94fe459381837
ATIFSHAH159 <aatifshah15@gmail.com>
2025-10-21 15:52:28 +0500

    CORS Issues

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,5 +1,8 @@
 import app from '../app.js';
 
+// Add some debugging
+console.log('API handler loaded for Vercel');
+
 // Export the Express app as the default handler for Vercel
 export default app;
 
