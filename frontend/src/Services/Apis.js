9b74cd2d957aae42151e18cbcf2f319e532685ea
ATIFSHAH159 <aatifshah15@gmail.com>
2025-10-21 15:07:25 +0500

    deployment

--- frontend/src/Services/Apis.js
+++ frontend/src/Services/Apis.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'my-portfolio-2025-lilac.vercel.app';
 
 // Send contact email
 export const sendContactEmail = async (emailData) => {
